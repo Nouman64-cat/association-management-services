c6b623114c26da8c233f40e58c83fab6b5c509ec
Nouman64-cat <noumanejazghost@gmail.com>
2024-11-09 01:29:58 +0500

    mobile responsive navbar issue resolve

--- app/components/navbar/BaseNavbar.tsx
+++ app/components/navbar/BaseNavbar.tsx
@@ -1,24 +1,27 @@
-"use client"
-import React, { useState } from 'react';
-import DesktopNavbar from './DesktopNavbar';
-import MobileNavbar from './MobileNavbar';
-
+"use client";
+import React, { useState, useEffect } from "react";
+import DesktopNavbar from "./DesktopNavbar";
+import MobileNavbar from "./MobileNavbar";
 
 const BaseNavbar: React.FC = () => {
   const [isMobile, setIsMobile] = useState<boolean>(false);
 
-  React.useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth <= 768);
-    handleResize(); // Set initial value
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+  useEffect(() => {
+    // Use matchMedia to set initial state and add listener
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    const handleResize = () => setIsMobile(mediaQuery.matches);
+
+    // Set initial state
+    handleResize();
+
+    // Add listener with matchMedia
+    mediaQuery.addEventListener("change", handleResize);
+
+    // Cleanup listener on component unmount
+    return () => mediaQuery.removeEventListener("change", handleResize);
   }, []);
 
-  return (
-    <header className='font-axiforma'>
-      {isMobile ? <MobileNavbar /> : <DesktopNavbar />}
-    </header>
-  );
+  return <header className="font-axiforma">{isMobile ? <MobileNavbar /> : <DesktopNavbar />}</header>;
 };
 
 export default BaseNavbar;
